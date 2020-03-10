2e77521faefa4cf16db1a77844582cc99db5cd13
pyrbin <31902695+pyrbin@users.noreply.github.com>
2020-03-10 09:59:03 +0100

    test ecs move system + components

---
+++ Assets/scenes/MovementTest/Selected.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Mathematics;
+
+/// <summary>
+/// Selected
+/// </summary>
+[GenerateAuthoringComponent]
+public class Selected : IComponentData
+{ }
---
+++ Assets/scenes/MovementTest/Speed.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+
+/// <summary>
+/// Speed
+/// </summary>
+[GenerateAuthoringComponent]
+public class Speed : IComponentData
+{
+    public float Value;
+}
