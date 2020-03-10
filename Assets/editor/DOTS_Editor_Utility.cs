4a6745eae56361c61d15ea4f7ee7a402884dbca2
pyrbin <31902695+pyrbin@users.noreply.github.com>
2020-03-10 02:27:47 +0100

    added DOTS editor utility

---
+++ Assets/editor/DOTS_Editor_Utility.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class DOTS_Editor_Utility : MonoBehaviour
+{
+    private const string COMPONENT_DATA_SCRIPT_PATH = "Assets/editor/dots_template/ComponentData.cs.txt";
+    private const string SIMPLE_SYSTEM_SCRIPT_PATH = "Assets/editor/dots_template/SimpleSystem.cs.txt";
+    private const string JOB_SYSTEM_SCRIPT_PATH = "Assets/editor/dots_template/JobSystem.cs.txt";
+
+    [MenuItem(itemName: "Assets/Create/DOTS/Objects/Component", isValidateFunction: false, priority: 1)]
+    public static void CreateComponentDataScript()
+    {
+        ProjectWindowUtil.CreateScriptAssetFromTemplateFile(COMPONENT_DATA_SCRIPT_PATH, "ComponentData.cs");
+    }
+
+    [MenuItem(itemName: "Assets/Create/DOTS/Objects/System", isValidateFunction: false, priority: 2)]
+    public static void CreateSimpleSystemScript()
+    {
+        ProjectWindowUtil.CreateScriptAssetFromTemplateFile(SIMPLE_SYSTEM_SCRIPT_PATH, "SimpleSystem.cs");
+    }
+
+    [MenuItem(itemName: "Assets/Create/DOTS/Objects/Job System", isValidateFunction: false, priority: 3)]
+    public static void CreateJobSystemScript()
+    {
+        ProjectWindowUtil.CreateScriptAssetFromTemplateFile(JOB_SYSTEM_SCRIPT_PATH, "ScheduleSystem.cs");
+    }
+}
